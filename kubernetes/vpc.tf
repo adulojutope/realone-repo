bfbaff52fb5bb209fd57568144cdc4aafcd7489a
adulojutope <Adulojutope3016@gmail.com>
2025-05-02 03:04:04 -0500

    Update vpc.tf

--- kubernetes/vpc.tf
+++ kubernetes/vpc.tf
@@ -2,9 +2,6 @@ provider "aws" {
     region = "us-east-1"
 }
 
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
 
 data "aws_availability_zones" "available" {}
 
